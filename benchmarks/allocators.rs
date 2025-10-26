849c2d6b74fcef00515175a1625a7f5766d735df
Azalea Colburn <azaleacolburn@gmail.com>
2025-10-26 16:44:40 -0700

    fix: remove optimizations libc benchmarking calls

--- benchmarks/allocators.rs
+++ benchmarks/allocators.rs
@@ -14,17 +14,17 @@ fn linked_alloc(b: &mut test::Bencher) {
     let layout = Layout::new::<[u8; 5000]>();
     b.iter(|| unsafe {
         let chunk = allocator.alloc(layout);
-        // chunk.write_bytes(10, 5000);
+        test::black_box(&chunk);
     });
 }
 
 #[bench]
-fn linked_dealloc(b: &mut test::Bencher) {
+fn linked_alloc_free(b: &mut test::Bencher) {
     let allocator = LinkedListAllocator::<'_, YerbaPageAllocator>::new();
     let layout = Layout::new::<[u8; 5000]>();
-    let chunk = unsafe { allocator.alloc(layout) };
-    assert!(!chunk.is_null());
     b.iter(|| unsafe {
+        let chunk = unsafe { allocator.alloc(layout) };
+        test::black_box(chunk);
         allocator.dealloc(chunk, layout);
     });
 }
@@ -36,26 +36,24 @@ fn stack_alloc(b: &mut test::Bencher) {
 
     b.iter(|| unsafe {
         let t = allocator.alloc(layout);
+        test::black_box(t);
     })
 }
 
 #[bench]
+#[unsafe(no_mangle)]
 fn c_malloc(b: &mut test::Bencher) {
     b.iter(|| unsafe {
-        for i in 0..100 {
-            let t: *mut u8 = libc::calloc(5000, 1).cast();
-            t.write_bytes(10, 5000);
-            for j in 0..5000 {
-                println!("{}", t.wrapping_add(j).read());
-            }
-        }
+        let t: *mut u8 = libc::calloc(5000, 1).cast();
+        test::black_box(&t);
     });
 }
 
 #[bench]
-fn c_free(b: &mut test::Bencher) {
-    let t = unsafe { libc::calloc(5000, 1) };
+fn c_malloc_free(b: &mut test::Bencher) {
     b.iter(|| unsafe {
+        let t = unsafe { libc::calloc(5000, 1) };
+        test::black_box(&t);
         libc::free(t);
     });
 }
